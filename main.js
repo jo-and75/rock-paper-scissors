f2818de24109a37ffbcf3b8c195e715f51b0d951
jo-and75 <jaotika@gmail.com>
2024-04-25 17:52:51 +0300

    Add getHumanChoice function

--- main.js
+++ main.js
@@ -4,4 +4,23 @@ console.log("Hello, World!")
 let getComputerChoice = ["rock","paper","scissors"][Math.floor(Math.random()*3)];
 
 console.log(getComputerChoice); 
-console.log(getComputerChoice)
+
+let getHumanChoice = prompt("Which weapon are you choosing today?") 
+switch(getHumanChoice){ 
+    case "rock": 
+    console.log(); 
+    break; 
+
+    case "paper": 
+    console.log(); 
+    break;  
+
+    case "scissors": 
+    console.log(); 
+    break;  
+
+    default: 
+    console.log("This doesn't run"); 
+}
+
+console.log(getHumanChoice); 
