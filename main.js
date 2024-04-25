aba3fb4dc1982d90da9c5b959419ef2c7225a9b8
jo-and75 <jaotika@gmail.com>
2024-04-25 19:02:42 +0300

    Add playRound function
    Generates automatic message depending on result of the game.

--- main.js
+++ main.js
@@ -6,23 +6,33 @@ let getComputerChoice = ["rock","paper","scissors"][Math.floor(Math.random()*3)]
 console.log(getComputerChoice); 
 
 let getHumanChoice = prompt("Which weapon are you choosing today?").toLowerCase()
-switch(getHumanChoice){ 
-    case "rock": 
-    console.log(); 
-    break; 
+    switch(getHumanChoice){ 
+        case "rock": 
+        console.log(); 
+        break; 
 
-    case "paper": 
-    console.log(); 
-    break;  
+        case "paper": 
+        console.log(); 
+        break;  
 
-    case "scissors": 
-    console.log(); 
-    break;  
+        case "scissors": 
+        console.log(); 
+        break;  
 
-    default: 
-    console.log("This doesn't run"); 
-}
+        default: 
+        console.log("This doesn't run"); 
+    }
 
 console.log(getHumanChoice);  
 
- 
+function playRound(humanChoice,computerChoice){ 
+if(humanSelection>computerSelection) { 
+    console.log("You Win!!!" + " " + humanSelection + "beats" + " " + computerSelection + ".")
+} else{ 
+    console.log("You lose!" + " " + computerSelection + "beats" + " " + humanSelection + ".")
+}
+
+} 
+
+const humanSelection = getHumanChoice().toLowerCase(); 
+const computerSelection = getComputerChoice(); 
