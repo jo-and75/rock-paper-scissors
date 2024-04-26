8b5bceddaebc1ff6b9d3a3ac44574471f455aaa3
jo-and75 <jaotika@gmail.com>
2024-04-26 19:47:37 +0300

    Removed humanChoice and computerChoice from global scope
    This is because they would lead to code stopping as they were run first

--- main.js
+++ main.js
@@ -1,57 +1,32 @@
 console.log("Welcome, 🤘📄✂️") 
-
-
-let getComputerChoice = ["rock","paper","scissors"][Math.floor(Math.random()*3)];
-
-let getHumanChoice = prompt("Please select either rock, paper, or scissors. 🤘📄✂️").toLowerCase();
-    switch(getHumanChoice){ 
-        case "rock": 
-        break; 
-
-        case "paper":  
-        break;  
-
-        case "scissors":  
-        break;   
-    }
-
+ 
 let humanScore = 0; 
-let computerScore = 0; 
+let computerScore = 0;
 
-function playRound(humanChoice,computerChoice){  
-    if(humanSelection == "rock" && computerSelection == "rock"||humanSelection == "paper" && computerSelection == "paper"||humanSelection == "scissors" && computerSelection == "scissors"){ 
+let playGame = function(rounds){    
+
+    for(let i = 0; i < rounds; i++){ 
+        const humanSelection = prompt("Please select either rock, paper, or scissors. 🤘📄✂️").toLowerCase();
+        const computerSelection = ["rock","paper","scissors"][Math.floor(Math.random()*3)];  
+        playRound(humanSelection,computerSelection); 
+        } 
+    
+    function playRound(humanSelection,computerSelection){   
+
+        if(humanSelection === computerSelection){ 
         console.log("It's a tie. Both chose"+ " "+ humanSelection); 
-    }else if(humanSelection == "rock" && computerSelection == "scissors"||humanSelection == "scissors" && computerSelection == "paper"||humanSelection == "paper" && computerSelection == "rock"){    
-        console.log("You Win!!!" + " " + humanSelection + " " + "beats" + " " + computerSelection + "."); 
-        console.log("Your Score:" + " " + ++humanScore + " " + "&" + " " + "Computer Score:"+ " "+ computerScore);   
-    }else if(humanSelection.toLowerCase !== "rock"||humanSelection.toLowerCase !=="paper"||humanSelection.toLowerCase !== "scissors"){
-        console.log("Warning: The provided response is not valid. Please ensure your input adheres to the expected format or guidelines. 🚫🔍") 
-    }else{    
-        console.log("You lose!" + " " + computerSelection + " " + "beats" + " " + humanSelection + "."); 
-        console.log("Computer Score:" + " " + ++computerScore + " " + "&" + " " + "Your Score:"+ " "+ humanScore); 
-    }   
-} 
-  
-const humanSelection = getHumanChoice; 
-const computerSelection = getComputerChoice; 
-
-playRound(humanSelection,computerSelection);  
-
-function playGame(){     
-
-    let humanScore = 0; 
-    let computerScore = 0; 
-
-    function playRound(humanChoice,computerChoice){  
-        if(humanSelection == "rock" && computerSelection == "rock"||humanSelection == "paper" && computerSelection == "paper"||humanSelection == "scissors" && computerSelection == "scissors"){ 
-            console.log("It's a tie, play again"); 
-        }else if(humanSelection == "rock" && computerSelection == "scissors"||humanSelection == "scissors" && computerSelection == "paper"||humanSelection == "paper" && computerSelection == "rock"){    
+        }else if(humanSelection == "rock" && computerSelection == "scissors"||
+        humanSelection == "scissors" && computerSelection == "paper"||
+        humanSelection == "paper" && computerSelection == "rock"){    
             console.log("You Win!!!" + " " + humanSelection + " " + "beats" + " " + computerSelection + "."); 
             console.log("Your Score:" + " " + ++humanScore + " " + "&" + " " + "Computer Score:"+ " "+ computerScore);   
-        }else{  
+        }else if(humanSelection.toLowerCase() !== "rock" && humanSelection.toLowerCase() !=="paper" && humanSelection.toLowerCase() !== "scissors"){
+            console.log("Warning: The provided response is not valid. Please ensure your input adheres to the expected format or guidelines. 🚫🔍") 
+        }else{    
             console.log("You lose!" + " " + computerSelection + " " + "beats" + " " + humanSelection + "."); 
             console.log("Computer Score:" + " " + ++computerScore + " " + "&" + " " + "Your Score:"+ " "+ humanScore); 
         }   
     }    
 } 
 
+playGame(5);
